fa32504f3ea4a5c316fdbb55d7ff2f93422e00df
MoBoustta <moboustta6@gmail.com>
2021-01-09 00:18:13 +0100

    update phpmyadmin setup file

--- srcs/phpmyadmin/srcs/start.sh
+++ srcs/phpmyadmin/srcs/start.sh
@@ -1,5 +1,13 @@
-#adduser -G www-data -D www-data
-#chown -R www-data:www-data /var/www/phpmyadmin
+#! /bin/bash
+
+#wget  http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
+mv phpMyAdmin-5.0.2-all-languages/* /var/www/phpmyadmin
+cp config.inc.php /var/www/phpmyadmin
+rm -rf /var/www/phpmyadmin/config.sample.inc.php
+adduser -G www-data -D www-data
+chown -R www-data:www-data /var/www/phpmyadmin
+#telegraf &
 #php-fpm7 --nodaemonize  &
-#nginx -g 'daemon off;' 
+#nginx -g 'daemon off;'
 supervisord -c /etc/supervisord.conf & tail -f /dev/null
