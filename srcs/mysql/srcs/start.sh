3525bd157c49ac06b4bf9952cb8b2331cb86a38d
Spirox <50895527+MoBoustta@users.noreply.github.com>
2021-01-16 21:00:10 +0100

    Main https (#1)
    * add https to nginx
    
    * add https to nginx
    
    * update nginx set up
    
    * update all Dockerfiles
    
    * set up telegraf
    
    * set up telegraf
    
    * fix the bug -> Database not found by mysql
    
    Co-authored-by: Mohammad  Boustta <moboustt@e2r9p4.1337.ma>

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -11,11 +11,8 @@ exec /usr/bin/mysqld -u root --console &
 sleep 5
 
 
-# #create wp db if not already exists
-# if ! mysql -u root -e 'USE wordpress'; then
-# 	mysql -u root -e 'CREATE DATABASE wordpress;'
-# 	# mysql -u root wordpress < /wordpress.sql
-# fi
+mysql -u root -e 'CREATE DATABASE wordpress;'
+mysql -u root wordpress < /wordpress.sql
 
 #create db admin
 mysql -u root -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'passwd';GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;USE wordpress;FLUSH PRIVILEGES;"
