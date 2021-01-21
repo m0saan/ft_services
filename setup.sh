f18854ad18dd30c555128987aaff57a37b6f98ea
MoBoustta <moboustta6@gmail.com>
2021-01-21 18:48:05 +0100

    Update building a setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!bin/bash
 
-eval $(minikube env default)
-
 # Delete And Kill Currently Working Processes
 minikube delete
 killall -TERM kubectl minikube VBoxHeadless
 
+eval $(minikube docker-env)
+
 # Start Minikube
 minikube start --driver=docker
 
+# Set up Metallb
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+
 # Build Docker Images
 
-docker build -t i_wordpress srcs/wordpress > tail -f dev/null/
+docker build -t i_wordpress srcs/wordpress
