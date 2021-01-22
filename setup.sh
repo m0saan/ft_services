8aa55a917bdfe4828447ed333492985a8260f67a
Mohammad  Boustta <moboustt@e2r9p4.1337.ma>
2021-01-22 18:59:02 +0100

    set minikube driver to virtualbox and build docker images

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ killall -TERM kubectl minikube VBoxHeadless
 
 
 # Start Minikube
-minikube start --driver=docker
+minikube start --driver=virtualbox
 
 eval $(minikube docker-env)
 
@@ -15,8 +15,12 @@ eval $(minikube docker-env)
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
+kubectl apply -f srcs/yaml/metalLb.yaml
 
 # Build Docker Images
 
-docker build -t i_wordpress srcs/wordpress
+
+docker build -t wordpress-image srcs/wordpress
+docker build -t phpmyadmin-image srcs/phpmyadmin
+docker build -t nginx-image srcs/nginx
+
