6b2792425a614718885fd92f27f671d6c7a842d9
MoBoustta <moboustta6@gmail.com>
2021-01-25 12:37:37 +0100

    update set up script for mysql

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -3,7 +3,10 @@
 rm -rf /var/cache/apk/* /etc/my.cnf*
 
 #create necessary folders
-mkdir -p /var/lib/mysql /run/mysqld
+mkdir -p /var/lib/mysql /run/mysqld /usr/share/phpmyadmin/tmp
+
+chmod 0777 /usr/share/phpmyadmin/tmp/
+
 
 #install and start services
 mysql_install_db -u root > /dev/null
@@ -12,10 +15,10 @@ sleep 5
 
 
 mysql -u root -e 'CREATE DATABASE wordpress;'
-mysql -u root wordpress < /wordpress.sql
 
 #create db admin
-mysql -u root -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'passwd';GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;USE wordpress;FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE USER 'root'@'%' IDENTIFIED BY 'root';GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;USE wordpress;FLUSH PRIVILEGES;"
+mysql -u root wordpress < /wordpress.sql
 
-#avoid container to stop
+#make sure container not to stop
 sleep infinity
