fc4914ad212d4e52a1a22984a313f06687e5a25c
MoBoustta <moboustta6@gmail.com>
2021-01-25 12:38:24 +0100

    update wordpress image files

--- srcs/wordpress/srcs/wp-config.php
+++ srcs/wordpress/srcs/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '10.102.44.148' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
