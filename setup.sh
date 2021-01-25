9a217112357cbce74ff50df43591469dd3b3cb55
MoBoustta <moboustta6@gmail.com>
2021-01-25 12:33:29 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ killall -TERM kubectl minikube VBoxHeadless
 
 
 # Start Minikube
-minikube start --driver=hyperkit
+# minikube start --driver=hyperkit
+minikube start --driver=virtualbox
+
 
 # Set up Metallb
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
@@ -14,9 +16,11 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/yaml/metalLb.yaml
 
+eval $(minikube docker-env)
+
 # Build Docker Images
 docker build -t wordpress-image srcs/wordpress
-docker build -t phpmyadmin-image srcs/phpmyadmin
 docker build -t nginx-image srcs/nginx
+docker build -t phpmyadmin-image srcs/phpmyadmin
+docker build -t mysql-image srcs/mysql
 
-eval $(minikube docker-env)
