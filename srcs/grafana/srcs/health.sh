6aec2675ae9be796e5a114db3fa327e5648e9fb6
MoBoustta <moboustta6@gmail.com>
2021-02-18 10:26:46 +0100

    fix liveness prob issues

--- srcs/grafana/srcs/health.sh
+++ srcs/grafana/srcs/health.sh
@@ -1,7 +1,7 @@
 if [[ $(ps | grep -v "grep" | grep -c "telegraf") == 0 ]]
 then
 	exit 1
-elif [[ $(ps | grep -v "grep" | grep -c "grafana-server") == 0 ]]
+elif [[ $(ps | grep -v "grep" | grep -c "./grafana-server") == 0 ]]
 then
 	exit 1
 else
